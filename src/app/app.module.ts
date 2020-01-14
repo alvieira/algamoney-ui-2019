f39b380af0666f115d538a34ffc90ccb1a645ecf
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-01-13 21:21:21 -0300

    desafio pesquisa de pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import {PaginatorModule} from 'primeng/paginator';
 import {TooltipModule} from 'primeng/tooltip';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
-    NavbarComponent
+    NavbarComponent,
+    PessoasPesquisaComponent
   ],
   imports: [
     BrowserModule,
