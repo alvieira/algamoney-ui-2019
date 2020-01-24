4519b7184c059b8b3ee466fba4771cea361807eb
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-01-23 21:41:11 -0300

    pipes

--- src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -8,16 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class PessoasPesquisaComponent implements OnInit {
 
   pessoas = [
-    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia ', estado: 'MG',
-      ativo: true},
-      { nome: 'Sebastião da Silva', cidade: 'São Paulo ', estado: 'SP',
-      ativo: false},
-      { nome: 'Carla Souza', cidade: 'Florianópolis ', estado: 'SC',
-      ativo: true},
-      { nome: 'Luís Pereira', cidade: 'Curitiba ', estado: 'PR',
-      ativo: true},
-      { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro ', estado: 'RJ',
-      ativo: false}
+    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia ', estado: 'MG', ativo: true},
+      { nome: 'Sebastião da Silva', cidade: 'São Paulo ', estado: 'SP', ativo: false},
+      { nome: 'Carla Souza', cidade: 'Florianópolis ', estado: 'SC', ativo: true},
+      { nome: 'Luís Pereira', cidade: 'Curitiba ', estado: 'PR', ativo: true},
+      { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro ', estado: 'RJ', ativo: false},
+      { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
   ];
 
   constructor() { }
